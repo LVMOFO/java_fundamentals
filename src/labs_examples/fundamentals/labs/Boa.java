f91056d7533591feb79829205c696ec99c9c5808
Bo <exexstream@gmail.com>
2022-05-13 07:41:27 -0400

    Submitted first project to CodingNomads

--- src/labs_examples/fundamentals/labs/Boa.java
+++ src/labs_examples/fundamentals/labs/Boa.java
@@ -1,2 +1,13 @@
-package labs_examples.fundamentals.labs;public class Boa {
+package labs_examples.fundamentals.labs;
+
+public class Boa {
+    public static void main(String[] args) {
+        int result = multiply(13, 13);
+        System.out.println("2400 Pushups so far in May! 13 * 13 = " + result);
+
+    }
+    public static int multiply(int a, int b) {
+        int result = a * b;
+        return result;
+    }
 }
