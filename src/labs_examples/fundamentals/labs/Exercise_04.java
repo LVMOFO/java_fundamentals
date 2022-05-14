001383aca20dcf38830973cc740f82c8d0ea7818
Bo <exexstream@gmail.com>
2022-05-14 15:05:47 -0400

    Labs: Fundamentals

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,23 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        int iInt = 4;
+        byte bByte = 1;
+        short sShort = 32767;
+        long lLong = 8000000000000000000l;
+        float fFloat = 9.1234567f;
+        double dDouble = 8.012345678901234;
+        boolean bBoolean = true;
+        char cChar = '2';
+
+        System.out.println("iInt = " + iInt);
+        System.out.println("bByte = " + bByte);
+        System.out.println("sShort = " + sShort);
+        System.out.println("lLong = " + lLong);
+        System.out.println("fFloat = " + fFloat);
+        System.out.println("dDouble = " + dDouble);
+        System.out.println("bBoolean = " + bBoolean);
+        System.out.println("cChar = " + cChar);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,23 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int iLength = str.length();
+        System.out.println("hello! is how many characters = " + iLength);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean bEquiv = str.equals(str2);
+        System.out.println(str + " is equivalent to " + str2 + " = " + bEquiv);
+        
         // please concatenate str & str2 and set the result to a new String variable below
+        String str3 = str + str2;
+        System.out.println("Concat of str and str2 = " + str3);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String str4 = str.toUpperCase();
+        System.out.println("Upside down Hi = " + str.substring(iLength-1) + str4.charAt(0));
+
 
     }
 
