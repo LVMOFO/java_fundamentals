24316663acea159d19903e8a991e6ca489b6de8f
Bo <exexstream@gmail.com>
2022-05-24 14:26:49 -0400

    Forgot Exercise03

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_03.java
@@ -10,11 +10,17 @@ package labs_examples.objects_classes_methods.labs.methods;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-//        int x = factorial(5);
-//        System.out.println(x);
-//        should print 120
+        int x = factorial(5);
+        System.out.println(x);
+        //should print 120
     }
-
+    public static int factorial(int a){
+        int result;
+        if(a == 1)
+            return 1;
+        result = factorial(a - 1) * a;
+        return result;
+    }
 
 
 
