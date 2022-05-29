97a330071c3682d6265e702992527a437a3b50b6
Bo <exexstream@gmail.com>
2022-05-29 10:00:13 -0400

    P001

--- src/labs_examples/objects_classes_methods/labs/objects/Pilot.java
+++ src/labs_examples/objects_classes_methods/labs/objects/Pilot.java
@@ -1,2 +1,81 @@
-package labs_examples.objects_classes_methods.labs.objects;public class Pilot {
+package labs_examples.objects_classes_methods.labs.objects;
+
+import java.util.Arrays;
+
+public class Pilot {
+    int height;
+    float weight;
+    float yearExperience;
+    boolean multiLingual;
+    String[] languages;
+    float salary;
+
+    public Pilot(int height, float weight, float yearExperience, boolean multiLingual, String[] languages, float salary) {
+        this.height = height;
+        this.weight = weight;
+        this.yearExperience = yearExperience;
+        this.multiLingual = multiLingual;
+        this.languages = languages;
+        this.salary = salary;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getYearExperience() {
+        return yearExperience;
+    }
+
+    public void setYearExperience(float yearExperience) {
+        this.yearExperience = yearExperience;
+    }
+
+    public boolean isMultiLingual() {
+        return multiLingual;
+    }
+
+    public void setMultiLingual(boolean multiLingual) {
+        this.multiLingual = multiLingual;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Pilot{" +
+                "height=" + height +
+                ", weight=" + weight +
+                ", yearExperience=" + yearExperience +
+                ", multiLingual=" + multiLingual +
+                ", languages=" + Arrays.toString(languages) +
+                ", salary=" + salary +
+                '}';
+    }
 }
--- src/zPractice/Phone.java
+++ src/zPractice/Phone.java
@@ -1,2 +1,57 @@
-package zPractice;public class Phone {
+package zPractice;
+
+public class Phone {
+    String brand;
+    String type;
+    String color;
+    int gbStoreage;
+
+    public Phone(String brand, String type, String color, int gbStoreage) {
+        this.brand = brand;
+        this.type = type;
+        this.color = color;
+        this.gbStoreage = gbStoreage;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getGbStoreage() {
+        return gbStoreage;
+    }
+
+    public void setGbStoreage(int gbStoreage) {
+        this.gbStoreage = gbStoreage;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "brand='" + brand + '\'' +
+                ", type='" + type + '\'' +
+                ", color='" + color + '\'' +
+                ", gbStoreage=" + gbStoreage +
+                '}';
+    }
 }
